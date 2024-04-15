c11322668e7864f4bd2c188f19b8e191799cbc0d
Victor Herguedas Martin <mac-victor.herguedas@autentias-MacBook-Pro-2.local>
2024-04-15 20:59:14 +0200

    Before prettier

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-console.log('hola')
-// import express, { Request, Response } from 'express'
-// import { SERVER_PORT } from "./utilities/environment.js"
+import express, { Request, Response } from 'express'
+import chalk from 'chalk'
+import { SERVER_PORT } from "./utilities/environment.js"
 
-// const app = express()
+const app = express()
 
-// app.get('/', (req: Request, res: Response) => {
-//   res.send("Hello world")
-// })
+app.get('/', (req: Request, res: Response) => {
+  res.send("Hello world")
+})
 
-// app.listen(SERVER_PORT, () => {
-//   console.log(`Server running on ${SERVER_PORT}`)
-// })
+app.listen(SERVER_PORT, () => {
+  console.log(`Server running on ${chalk.blue(`http://localhost:${SERVER_PORT}`)}`)
+})
